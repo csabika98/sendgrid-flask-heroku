e2530d745c809a1505af49220985df6021663600
Csaba Sallai <68144659+csabika98@users.noreply.github.com>
2023-10-09 14:04:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ app = Flask(__name__)
 def mail():
     if request.method == "POST":
         sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
-        from_email = Email(request.form.get("from_email"))
-        to_email = Email(request.form.get("to_email"))
-        subject = request.form.get("subject")
-        content = Content("text/plain", request.form.get("content"))
+        from_email = "6ix9inegotti@naver.com"
+        to_email = "sallcsa.csaba8@gmail.com"
+        subject = "Test"
+        content = "Test"
         mail = Mail(from_email, subject, to_email, content)
         response = sg.client.mail.send.post(request_body=mail.get())
         if response.status_code == 202:
